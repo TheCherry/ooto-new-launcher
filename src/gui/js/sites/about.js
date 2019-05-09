eab6e668f6b7ca39278e4781544abec3b92de7b8
TheCherry <deekaybee0@gmail.com>
2019-05-09 17:57:21 +0100

    about site

---
+++ src/gui/js/sites/about.js
@@ -0,0 +1,16 @@
+class About extends BaseSite {
+  constructor() {
+    super();
+    this.html_file = "about.html"
+    //called by init
+  }
+
+  onload(){
+    //called then the site gets first entered
+  }
+
+  onenter(){
+    //called then the site gets entered
+  }
+}
+new About();
